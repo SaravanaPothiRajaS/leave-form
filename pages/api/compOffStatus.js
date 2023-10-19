6fc5a9e0ae075ca14abac77ce5f94ab4153331c8
Edwin <edwinraj1462003@gmail.com>
2023-10-19 10:37:42 +0530

    edwinoct18

--- pages/api/compOffStatus.js
+++ pages/api/compOffStatus.js
@@ -3,28 +3,29 @@ import authenticateToken from '@/app/middleware';
 const fs = require('fs').promises;
 export default async (req, res) => {
   try {
-    authenticateToken(req, res, async(isAuthenticated) => {
+    authenticateToken(req, res, async (isAuthenticated) => {
       if (isAuthenticated) {
-    const data = await fs.readFile('compOffStatus.json', 'utf8');
+        const data = await fs.readFile('compOffStatus.json', 'utf8');
         const jsonData = JSON.parse(data);
-      
-        const { department ,role} = req.body;
+
+        const { department, role } = req.body;
         console.log(department);
-        if(role==="approver"){
+        if (role === "approver") {
           const filteredData = jsonData.filter(item => item.role === role);
+          const compPending = filteredData.filter(item => item.status === "pending");
+          const compPendingCount = compPending.length;
 
-          if (filteredData.length > 0) {
-          
-            res.json(filteredData);
-          } 
-        }else if(role==="user"){
-          const filteredData = jsonData.filter(item => (item.role === role)&&(item.department===department));
 
-          if (filteredData.length > 0) {
-          
-            res.json(filteredData);
-          } 
-        } 
+          res.json({ filteredData: filteredData, compPendingCount: compPendingCount });
+        } else if (role === "user") {
+          const filteredData = jsonData.filter(item => (item.role === role) && (item.department === department));
+          const compPending = filteredData.filter(item => item.status === "pending");
+          const compPendingCount = compPending.length;
+          console.log(compPendingCount);
+
+
+          res.json({ filteredData: filteredData, compPendingCount: compPendingCount });
+        }
       } else {
         res.status(403).send('Forbidden: Invalid Token');
       }
